28fd3b1d0fdd7d9d82999b95441c8fefcceaeb59
Aniruddha Adhikary <aniruddha@adhikary.net>
2022-05-04 08:39:49 +0800

    tests: fix default account test

--- core/budgeting/ynab.spec.ts
+++ core/budgeting/ynab.spec.ts
@@ -1,8 +1,8 @@
 import {determineAccount} from "./ynab";
 
 describe('determineAccount', () => {
-    it('defaults to DBS_CREDIT', async () => {
-        expect(await determineAccount({accountEnding: '3885'}, {'3886': 'DBS_DEBIT'})).toEqual('DBS_CREDIT');
+    it('defaults to DBS_DEBIT', async () => {
+        expect(await determineAccount({accountEnding: '3885'}, {'3886': 'DBS_DEBIT'})).toEqual('DBS_DEBIT');
     })
 
     it('returns correct config if configured', async () => {
